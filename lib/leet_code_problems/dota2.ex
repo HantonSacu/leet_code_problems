f9e321223f06ff09d4e5257d3f5063bdfa54c8e2
Kenneth Kostrešević <kenneth.kostresevic@gmail.com>
2023-05-05 10:31:34 +0200

    Implement solver

--- lib/leet_code_problems/dota2.ex
+++ lib/leet_code_problems/dota2.ex
@@ -1,5 +1,28 @@
 defmodule LeetCodeProblems.Dota2 do
   @spec predict_party_victory(senate :: String.t()) :: String.t()
   def predict_party_victory(senate) do
+    senate
+    |> String.graphemes()
+    |> solver()
+  end
+
+  defp solver(["R" | tail]) do
+    case List.delete(tail, "D") do
+      [] ->
+        "Radiant"
+
+      rest ->
+        if Enum.all?(rest, &(&1 == "R")), do: "Radiant", else: solver(rest ++ ["R"])
+    end
+  end
+
+  defp solver(["D" | tail]) do
+    case List.delete(tail, "R") do
+      [] ->
+        "Dire"
+
+      rest ->
+        if Enum.all?(rest, &(&1 == "D")), do: "Dire", else: solver(rest ++ ["D"])
+    end
   end
 end
